cf363f4c6aa36deb8406118cffe45a4fde2a3796
Matthew Mazzanti <matthew.mazzanti@gmail.com>
2023-08-25 08:39:08 -0400

    Add read secrets policy to ec2 instance

--- main.tf
+++ main.tf
@@ -13,8 +13,59 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_secretsmanager_secret" "test_secret" {
+  name = "test_secret"
+}
+
+resource "aws_secretsmanager_secret_version" "example" {
+  secret_id     = aws_secretsmanager_secret.test_secret.id
+  secret_string = "Hello world!"
+}
+
+resource "aws_iam_policy" "factorio_policy" {
+  name        = "factorio_policy"
+  path        = "/"
+  description = "Allow factorio server to read secrets"
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : ["secretsmanager:GetSecretValue"],
+        "Resource" : [aws_secretsmanager_secret.test_secret.id]
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role" "factorio_role" {
+  name = "factorio_role"
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Service" : "ec2.amazonaws.com"
+        },
+        "Action" : "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "factorio_attach" {
+  role       = aws_iam_role.factorio_role.name
+  policy_arn = aws_iam_policy.factorio_policy.arn
+}
+
+resource "aws_iam_instance_profile" "factorio_profile" {
+  name = "factorio_profile"
+  role = aws_iam_role.factorio_role.name
+}
+
 resource "aws_key_pair" "mmazzanti" {
-  key_name = "mmazzanti"
+  key_name   = "mmazzanti"
   public_key = file("${path.module}/keys/mmazzanti.pub")
 }
 
@@ -23,7 +74,8 @@ resource "aws_instance" "factorio" {
   instance_type = "t2.micro"
   key_name      = aws_key_pair.mmazzanti.key_name
 
-  user_data = file("${path.module}/configuration.nix")
+  user_data            = file("${path.module}/configuration.nix")
+  iam_instance_profile = aws_iam_instance_profile.factorio_profile.name
 
   tags = {
     Name = "factorio"
